54799ce4cbc828c676a6cbfa2239830203c65fc1
Anaël iMac <anael@barodine.fr>
2021-08-01 00:56:35 +0200

    feat: changed environment variable, adding prefix 'DISCORD_BOT_LAUNCHER_'
    Main launcher's variables are therefore prefixed with 'DISCORD_BOT_LAUNCHER_MAIN_', and individual bots' variable will be prefixed with 'DISCORD_BOT_LAUNCHER_BOT_{id}_'

--- src/main/java/fr/barodine/anael/discord/launcher/Launcher.java
+++ src/main/java/fr/barodine/anael/discord/launcher/Launcher.java
@@ -10,20 +10,20 @@ public class Launcher {
 
     public static void launch() {
         // Vérification que les variables d'environnement sont paramétrées.
-        String databaseDomain = System.getenv("MAIN_SQL_DATABASE_DOMAIN");
-        if (databaseDomain == null) System.err.println("Main database domain environment variable '" + "MAIN_SQL_DATABASE_DOMAIN" + "' is undefined or null");
+        String databaseDomain = System.getenv("DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_DOMAIN");
+        if (databaseDomain == null) System.err.println("Main database domain environment variable '" + "DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_DOMAIN" + "' is undefined or null");
 
-        String databasePort = System.getenv("MAIN_SQL_DATABASE_PORT");
-        if (databasePort == null) System.err.println("Main database port environment variable '" + "MAIN_SQL_DATABASE_PORT" + "' is undefined or null");
+        String databasePort = System.getenv("DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_PORT");
+        if (databasePort == null) System.err.println("Main database port environment variable '" + "DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_PORT" + "' is undefined or null");
 
-        String databaseDb = System.getenv("MAIN_SQL_DATABASE_DB");
-        if (databaseDb == null) System.err.println("Main database target database environment variable '" + "MAIN_SQL_DATABASE_DB" + "' is undefined or null");
+        String databaseDb = System.getenv("DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_DB");
+        if (databaseDb == null) System.err.println("Main database target database environment variable '" + "DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_DB" + "' is undefined or null");
 
-        String databaseUser = System.getenv("MAIN_SQL_DATABASE_USER");
-        if (databaseUser == null) System.err.println("Main database user environment variable '" + "MAIN_SQL_DATABASE_USER" + "' is undefined or null");
+        String databaseUser = System.getenv("DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_USER");
+        if (databaseUser == null) System.err.println("Main database user environment variable '" + "DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_USER" + "' is undefined or null");
 
-        String databasePasswd = System.getenv("MAIN_SQL_DATABASE_PASSWD");
-        if (databasePasswd == null) System.err.println("Main database password environment variable '" + "MAIN_SQL_DATABASE_PASSWD" + "' is undefined or null");
+        String databasePasswd = System.getenv("DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_PASSWD");
+        if (databasePasswd == null) System.err.println("Main database password environment variable '" + "DISCORD_BOT_LAUNCHER_MAIN_SQL_DATABASE_PASSWD" + "' is undefined or null");
 
         if (databaseDomain == null || databasePort == null || databaseDb == null || databaseUser == null || databasePasswd == null) {
             System.err.println("Please specify all above environment variables");
