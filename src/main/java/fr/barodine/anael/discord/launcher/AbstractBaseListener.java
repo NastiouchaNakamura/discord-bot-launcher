8f857e7efd53cd210df792ea0c0658a09458a27e
Anaël iMac <anael@barodine.fr>
2021-08-01 02:02:14 +0200

    feat: added easy variable getter for individual bots

--- src/main/java/fr/barodine/anael/discord/launcher/AbstractBaseListener.java
+++ src/main/java/fr/barodine/anael/discord/launcher/AbstractBaseListener.java
@@ -5,16 +5,26 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class AbstractBaseListener extends ListenerAdapter {
     // Attributs
     private final long idBot;
     private final JDA jda;
+    private final Map<String, String> variables;
 
     // Constructeurs
     public AbstractBaseListener(long idBot, @Nonnull final JDA jda) {
         this.idBot = idBot;
         this.jda = jda;
+        this.variables = new HashMap<>();
+        String prefix = "DISCORD_BOT_LAUNCHER_BOT_" + this.idBot + "_";
+        for (Map.Entry<String, String> variable : System.getenv().entrySet()) {
+            if (variable.getKey().startsWith(prefix)) {
+                variables.put(variable.getKey().substring(prefix.length()), variable.getValue());
+            }
+        }
     }
 
     // Getteurs
@@ -24,6 +34,10 @@ public abstract class AbstractBaseListener extends ListenerAdapter {
 
     public JDA getJda() {
         return this.jda;
+    }
+
+    public String getVariable(@Nonnull final String key) {
+        return this.variables.get(key);
     }
 
     // Méthodes
